40fbf1863527c1b61944808a016864a1c1b736fa
triptych <triptych@gmail.com>
2010-11-22 14:06:19 -0800

    More good stuff. getting saving to work.

--- src/web/js/ffort.js
+++ src/web/js/ffort.js
@@ -1,46 +1,79 @@
-YUI().use("node", function(Y){
+YUI().use("node", "gallery-storage-lite", function(Y){
   ffort = {
+    charData : {
+      name: "",
+      gender: "",
+      chr_class: ""
+    },
     init: function(){
       Y.log("ffort starting up!");
       ffort.bindings();
+      ffort.getCharData();
+      ffort.updateCharView();
     },
+    updateCharView: function(){
+      Y.log("update char view");
+       
+       
+       //Y.log(Y.one(".chr_name"));
+       //Y.one(".chr_name").set("value","foo");
+      Y.one(".chr_name").set("value", ffort.charData.name);
+      Y.one(".chr_gender").set("value",ffort.charData.gender);
+      Y.one(".chr_class").set("value", ffort.charData.chr_class);
+    },
+    getCharData: function(){
+      Y.log(ffort.charData);
+      
+      Y.StorageLite.on('storage-lite:ready', function () {
+       Y.log("storage lite ready -- getchardata"); 
+       if(Y.StorageLite.getItem("character") != null){
+         ffort.charData = Y.StorageLite.getItem("character",true);
+         //Y.log(Y.StorageLite.getItem("character",true))
+       }
+       
+      });
+    },
+    setCharData: function(){
+      //includes saving it
+      ffort.charData.name = Y.one(".chr_name").get("value");
+      ffort.charData.gender = Y.one(".chr_gender").get("value");
+      ffort.charData.chr_class=Y.one(".chr_class").get("value");
+      
+      Y.log(ffort.charData);
+      
+      //now save it:
+      Y.StorageLite.on('storage-lite:ready', function () {
+       Y.log("storage lite ready -- saving data"); 
+       Y.StorageLite.setItem("character", Y.JSON.stringify(ffort.charData));
+      });
+      
+      
+    },
+    
     bindings: function(){
       Y.log("bindings called");
       
-      Y.delegate("click",function(){
-        Y.log("clicked");
-        var tgtid = this.get("id");
+      Y.all(".ffort_save").on("click",function(e){
+        e.preventDefault();
+        Y.log("save clicked");
+        ffort.setCharData();
         
-        switch(tgtid){
-          case "home_link":
-            ffort.showPanel("home")   
-            break;
-          case "about_link":
-            ffort.showPanel("about")   
-            break;
-          case "character_link":
-            ffort.showPanel("character")   
-            break;
-          case "fortune_link":
-            ffort.showPanel("fortune")   
-            break;
-          default:
-            Y.log("something else clicked?");
-          
-        }
-      },"#fantfort .dash .toolbar", "li");
-    },
-    showPanel: function(pnl){
-      Y.log("showpanel " + pnl);
-      Y.all(".panel").addClass("hidden");
+      });
       
-      Y.one("."+pnl).removeClass("hidden");
+      Y.all(".ffort_clear").on("click",function(e){
+        e.preventDefault();
+        Y.log("clear clicked");
+      })
+
     }
+
   }
 
 
-
-  ffort.init();
+  Y.on("domready",function(){
+    ffort.init();
+  })
+  
     
 })
 
